57483bade5b78d42683b909d92a7268f2c3c3d5c
Harshit <soniharshit186@gmail.com>
2024-02-21 17:06:32 +0530

    bug ==

--- webapp/src/teacher/main.tsx
+++ webapp/src/teacher/main.tsx
@@ -243,7 +243,7 @@ const Sync = ({ setloading }: { setloading: React.Dispatch<React.SetStateAction<
     <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
       <p className="text-sm">Syncing...</p>
     </div>
-    {message == '' && <div className="bg-blue-100 border-t text-center border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+    {message === '' && <div className="bg-blue-100 border-t text-center border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
       <p className="text-sm">{message}</p>
     </div>}
   </>);
