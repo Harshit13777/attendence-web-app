12e686ed44de2869b010597fd3be909ce325b0d0
Harshit <soniharshit186@gmail.com>
2024-04-24 11:04:21 +0530

    update student api

--- webapp/src/static_api.tsx
+++ webapp/src/static_api.tsx
@@ -7,7 +7,7 @@ export function get_api() {
             //set student api
             'student_api': 'https://script.google.com/macros/s/AKfycbweGk1X69PSdUr5Hl5rLfHIS6Eoq1ENfZ_r4y3-LN72MDHqTqirs-rfmVXT-p4qg17-/exec',
             //set teacher api
-            'teacher_api': 'https://script.google.com/macros/s/AKfycbyMmJY9f1K9fPhRqdO-xXMrp_wkomhqvonYbdx5VwJGeO6zRk60dPlap7lFRSWhcZo_PA/exec'
+            'teacher_api': 'https://script.google.com/macros/s/AKfycbxTB1aYPltEFBs6f5dWcWAUpyRN7t4t48E6bqxCGeT8i8SS_35GDsYeBekKjCCF7fDwuw/exec'
 
         }
     )
